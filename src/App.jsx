7b8c4ab8b12a0627d828717c72575b1222a91b26
Arun Shekar <shekar.arun@gmail.com>
2025-06-10 19:41:35 +0530

    feat: Add component to render player information

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Player from "./components/Player"
 
 function App() {
 
 
   return (
-    <p>Coming soon!</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name={'Pl1'} symbol={'X'} />
+          <Player name={'Pl2'} symbol={'O'} />
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
   )
 }
 
